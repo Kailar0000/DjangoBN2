772df5957a9e83ba8e6762c7f24b59578d8c8f36
Kailar0000 <kailar0000@gmail.com>
2024-02-12 16:38:08 +0300

    'Models'

--- store/myapp/models.py
+++ store/myapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(blank=False)
+    direction = models.CharField(max_length=100, blank=False)
+    date = models.DateField(auto_now=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    price = models.DecimalField(max_digits=8, decimal_places=2, blank=False)
+    description = models.TextField(blank=False)
+    sum = models.IntegerField(blank=False)
+    date = models.DateField(auto_now=True)
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
