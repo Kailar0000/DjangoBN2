e0e4c385a15d4c6835576eb8730e9f2eafad246f
Kailar0000 <kailar0000@gmail.com>
2024-02-12 16:43:11 +0300

    'ModelsSave'

--- store/myapp/models.py
+++ store/myapp/models.py
@@ -6,6 +6,9 @@ class User(models.Model):
     direction = models.CharField(max_length=100, blank=False)
     date = models.DateField(auto_now=True)
 
+    def __str__(self):
+        return f'Username: {self.name}, email: {self.email}'
+
 class Product(models.Model):
     name = models.CharField(max_length=100, blank=False)
     price = models.DecimalField(max_digits=8, decimal_places=2, blank=False)
@@ -13,8 +16,14 @@ class Product(models.Model):
     sum = models.IntegerField(blank=False)
     date = models.DateField(auto_now=True)
 
+    def __str__(self):
+        return f'Username: {self.name}, email: {self.email}'
+
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
     date_ordered = models.DateTimeField(auto_now_add=True)
     total_price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return f'Username: {self.customer}, date_ordered: {self.date_ordered }'
